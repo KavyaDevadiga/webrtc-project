9153187011821f0eb95c2b7231782675f1ec2b37
KavyaDevadiga <kdevadiga74@gmail.com>
2024-09-09 00:22:36 +0530

    setup: completed dev setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { PORT } from "src/env";
-import middleware from "src/setup/middleware";
+import { PORT } from "@src/env";
+import middleware from "@src/setup/middleware";
 
 if (!PORT) {
   console.log(`No port value specified...`);
