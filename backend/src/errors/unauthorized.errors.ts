3063f7a398b30b42cf52a7ab0eed81cc7c1b3ebe
KavyaDevadiga <kdevadiga74@gmail.com>
2024-10-06 01:08:39 +0530

    setup: adds factory pattern for error handling

--- backend/src/errors/unauthorized.errors.ts
+++ backend/src/errors/unauthorized.errors.ts
@@ -2,7 +2,7 @@ import { BaseError } from "@src/errors";
 import { StatusCodes } from "http-status-codes";
 
 export class UnAuthorizedError extends BaseError {
-  constructor(message: string) {
-    super(message, StatusCodes.UNAUTHORIZED);
+  constructor() {
+    super("Unauthorized", StatusCodes.UNAUTHORIZED);
   }
 }
