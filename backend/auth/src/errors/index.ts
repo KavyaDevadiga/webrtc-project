ebb9f12b54a6cf36c52450b21edd43710309a8c9
KavyaDevadiga <kdevadiga74@gmail.com>
2024-10-24 00:43:29 +0530

    fixes: adds export internal server error in index.ts of error

--- backend/auth/src/errors/index.ts
+++ backend/auth/src/errors/index.ts
@@ -2,5 +2,6 @@ export { AppError } from "@src/errors/appError.errors";
 export { BadRequestError } from "@src/errors/badrequest.errors";
 export { BaseError } from "@src/errors/base.errors";
 export { ForbiddedError } from "@src/errors/forbidden.errors.ts";
+export { InternalServerError } from "@src/errors/internalServerError.errors";
 export { NotFoundError } from "@src/errors/notfound.errors";
 export { UnAuthorizedError } from "@src/errors/unauthorized.errors";
